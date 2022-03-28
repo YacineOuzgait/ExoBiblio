4acffe3d4c48761a0fb3a22cec10694e17203268
adam <adam.kassir@gmail.com>
2022-03-28 09:09:10 +0200

    maj classes vite fait

---
+++ classes/MotCle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoBiblio.classes
+{
+    internal class MotCle
+    {
+        int id;
+        string mot;
+
+        public MotCle(int id, string mot)
+        {
+            this.id = id;
+            this.mot = mot;
+        }
+
+    }
+}
