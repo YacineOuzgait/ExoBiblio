9a5ce03ff71f375c142eb78e2e3ea985f223cdd2
YacineOuzgait <yacineouzgait59146@gmail.com>
2022-03-28 11:17:49 +0200

    commit  classe livre

---
+++ classes/Livre.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoBiblio.classes
+{
+    public class Livre
+    {
+        int id;
+        string titre;
+        string isbn;
+        bool deleted;
+
+        public Livre(int id, string titre, string isbn, bool deleted)
+        {
+            this.id = id;
+            this.titre = titre;
+            this.isbn = isbn;
+            this.deleted = deleted;
+        }
+
+    }
+}
