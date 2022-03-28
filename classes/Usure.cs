cdeaa4f01cb6a7c6250e40ca4c6816dbcfdd6d57
YacineOuzgait <yacineouzgait59146@gmail.com>
2022-03-28 09:36:08 +0200

    commit Usure

---
+++ classes/Usure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoBiblio.classes
+{
+    internal class Usure
+    {
+        int id;
+        string etat;
+        bool deleted;
+
+        public Usure()
+        {
+
+        }
+
+        public Usure(int id, string etat, bool deleted)
+        {
+            this.id = id;
+            this.etat = etat;
+            this.deleted = deleted;
+        }
+
+    }
+}
