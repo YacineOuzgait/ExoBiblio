51f2921b03813bd69bfc9ff61c769459dd722781
GuillaumeBrunet59 <brunet.guillaume59267@gmail.com>
2022-03-29 16:23:26 +0200

    Ajout relation Exemplaire

--- classes/Exemplaire.cs
+++ classes/Exemplaire.cs
@@ -38,8 +38,119 @@ namespace ExoBiblio.classes
             }
         }
 
+        [JsonProperty(PropertyName = "id_livre")]
+        private int? idLivre;
+        public int? IdLivre
+        {
+            get { return idLivre; }
+            set
+            {
+                if (this.idLivre != value)
+                {
+                    this.idLivre = value;
+                    //TODO persist ?
+                }
+            }
+        }
 
+        [JsonIgnore]
+        private Livre livre;
+        public Livre Livre
+        {
+            get
+            {
+                if (this.livre == null)
+                {
+                    livre = Livre.jDA.GetById(this.idLivre);
+                }
+                return livre;
+            }
+            set
+            {
+                if (this.idLivre != value?.Id)
+                {
+                    Livre?.RemoveExemplaire(this);
+                    this.idLivre = value?.Id;
+                    this.livre = null; //need to reset Livre get
+                    Livre?.AddExemplaire(this);
+                }
+            }
+        }
 
+        [JsonProperty(PropertyName = "id_usure")]
+        private int idUsure;
+        public int IdUsure
+        {
+            get { return idUsure; }
+            set
+            {
+                if (this.idUsure != value)
+                {
+                    this.idUsure = value;
+                }
+            }
+        }
 
+        [JsonIgnore]
+        private Usure usure;
+        public Usure Usure
+        {
+            get
+            {
+                if (this.usure == null)
+                {
+                    usure = Usure.jDA.GetById(this.idUsure);
+                }
+                return usure;
+            }
+            set
+            {
+                if (this.idUsure != value?.Id)
+                {
+                    Usure?.RemoveExemplaire(this);
+                    this.idUsure = value?.Id;
+                    this.usure = null; //need to reset Usure get
+                    Usure?.AddExemplaire(this);
+                }
+            }
+        }
+
+        [JsonProperty(PropertyName = "id_editeur")]
+        private int idEditeur;
+        public int IdEditeur
+        {
+            get { return idEditeur; }
+            set
+            {
+                if (this.idEditeur != value)
+                {
+                    this.idEditeur = value;
+                }
+            }
+        }
+
+        [JsonIgnore]
+        private Editeur editeur;
+        public Editeur Editeur
+        {
+            get
+            {
+                if (this.editeur == null)
+                {
+                    editeur = Editeur.jDA.GetById(this.idEditeur);
+                }
+                return editeur;
+            }
+            set
+            {
+                if (this.idEditeur != value?.Id)
+                {
+                    Editeur?.RemoveExemplaire(this);
+                    this.idEditeur = value?.Id;
+                    this.editeur = null; //need to reset Editeur get
+                    Editeur?.AddExemplaire(this);
+                }
+            }
+        }
     }
 }
