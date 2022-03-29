714ddcfc19816b833784b1c8b991f158d3809da7
GuillaumeBrunet59 <brunet.guillaume59267@gmail.com>
2022-03-29 10:45:11 +0200

    MAJ classes Emprunt ,Exemplaire ,Usure

---
+++ classes/Exemplaire.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ExoBiblio.classes
+{
+    internal class Exemplaire : BaseModel<Exemplaire>
+    {
+        [JsonProperty(PropertyName = "date_achat")]
+        string dateAchat;
+        string emplacement;
+        
+
+        public DateTime DateAchat
+        {
+            get => DateTime.Parse(dateAchat);
+            set
+            {
+                if (this.dateAchat != value.ToString("yyyy-MM-dd"))
+                {
+                    this.dateAchat = value.ToString("yyyy-MM-dd");
+                }
+            }
+        }
+
+        public string Emplacement
+        {
+            get => emplacement;
+            set
+            {
+                if (this.emplacement != value)
+                {
+                    this.emplacement = value;
+                }
+            }
+        }
+
+
+
+
+    }
+}
