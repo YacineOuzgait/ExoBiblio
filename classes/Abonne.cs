9ea33e63bd3879d0c9edb40ef163bb8730b1f66e
adam <adam.kassir@gmail.com>
2022-03-30 12:02:23 +0200

    ajout relations  abonne

--- classes/Abonne.cs
+++ classes/Abonne.cs
@@ -124,6 +124,129 @@ namespace ExoBiblio.classes
                 }
             }
         }
+
+        [JsonProperty(PropertyName = "id_categorie")]
+        private int idCategorie;
+        public int IdCategorie
+        {
+            get { return idCategorie; }
+            set
+            {
+                if (this.idCategorie != value)
+                {
+                    this.idCategorie = value;
+                    //TODO persist ?
+                }
+            }
+        }
+
+        [JsonIgnore]
+        private Categorie categorie;
+        public Categorie Categorie
+        {
+            get
+            {
+                if (this.categorie == null)
+                {
+                    categorie = Categorie.jDA.GetById(this.idCategorie);
+                }
+                return categorie;
+            }
+            set
+            {
+                if (this.idCategorie != value.Id)
+                {
+                    Categorie.RemoveCategorie(this);
+                    this.idCategorie = value.Id;
+                    //this.categorie = null; //need to reset Livre get
+                    Categorie.AddCategorie(this);
+                }
+            }
+        }
+
+        //relation abonne.emprunts
+        [JsonIgnore]
+        private List<Emprunt> empruntsList;
+        public List<Emprunt> EmpruntsList
+        {
+            get
+            {
+                if (this.empruntsList == null)
+                {
+                    this.empruntsList = Emprunt.jDA.GetAll(item => item.IdAbonne == this.Id);
+                }
+                return this.empruntsList;
+            }
+        }
+        
+        public List<Emprunt> AddEmprunt(Emprunt emprunt)
+        {
+            if (this.EmpruntsList.Find(item => item.Id == emprunt.Id) == null)
+            {
+                this.EmpruntsList.Add(emprunt);
+                if (emprunt.Abonne.Id != this.Id)
+                {
+                    emprunt.Abonne = this;
+                }
+            }
+            return this.EmpruntsList;
+        }
+
+        public List<Emprunt> RemoveEmprunt(Emprunt emprunt)
+        {
+            int index = this.EmpruntsList.FindIndex(item => item.Id == emprunt.Id);
+            if (index >= 0)
+            {
+                this.EmpruntsList.RemoveAt(index);
+                if (emprunt.Abonne.Id == this.Id)
+                {
+                    emprunt.Abonne = null;
+                }
+            }
+            return this.EmpruntsList;
+        }
+
+        //relation abonne.reservations
+        [JsonIgnore]
+        private List<Reservation> reservationsList;
+        public List<Reservation> ReservationsList
+        {
+            get
+            {
+                if (this.reservationsList == null)
+                {
+                    this.reservationsList = Reservation.jDA.GetAll(item => item.IdAbonne == this.Id);
+                }
+                return this.reservationsList;
+            }
+        }
+        public List<Reservation> AddEmprunt(Reservation reservation)
+        {
+            if (this.ReservationsList.Find(item => item.Id == reservation.Id) == null)
+            {
+                this.ReservationsList.Add(reservation);
+                if (reservation.Abonne.Id != this.Id)
+                {
+                    reservation.Abonne = this;
+                }
+            }
+            return this.ReservationsList;
+        }
+
+        public List<Reservation> RemoveEmprunt(Reservation reservation)
+        {
+            int index = this.ReservationsList.FindIndex(item => item.Id == reservation.Id);
+            if (index >= 0)
+            {
+                this.ReservationsList.RemoveAt(index);
+                if (reservation.Abonne.Id == this.Id)
+                {
+                    reservation.Abonne = null;
+                }
+            }
+            return this.ReservationsList;
+        }
+
     }
 
 
